0ca057553c01d3216c91a1dfda10f7aea832ea50
heitor <146001697+gabriel-heitor@users.noreply.github.com>
2025-05-30 11:06:24 +0000

    aula 3

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 
 const botoes = document.querySelectorAll(".botoes");
 console.log(botoes)
+
+
 for(let i=0; i< botoes.length; i++){
      botoes[i].onclick = function(){
+
+
+        for (let j=0; j<botoes.length; j++){
+         botoes[j].remove.add("ativo");
+        }
         botoes[i].classList.add("ativo");
      } 
 
